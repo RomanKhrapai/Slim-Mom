3595bfedc22817d6ebc7659e178e84b01653afcc
gamabuntafrog <kirik.slaw@gmail.com>
2022-07-07 13:33:10 +0300

    Зробив компонент навігації

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import {useState, useEffect} from "react"
+import styles from 'navigation.module.scss';
+
+const Navigation = () => {
+
+
+
+    return (
+        <nav>
+
+        </nav>
+    )
+}
+
+export default Navigation
