d36088fabfbd417ec65fcee8f20eeb64911b2231
PiekharievaK <pechareva@gmail.com>
2022-07-09 00:53:26 +0300

    Добавляет временную БД пользователей

---
+++ src/Components/usersDB.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    userId: 1,
+    name: 'user1',
+    age: 27,
+    blood: 2,
+    current: 75,
+    height: 170,
+    desired: 65,
+  },
+  {
+    userId: 2,
+    name: 'user2',
+    age: 20,
+    blood: 4,
+    current: 80,
+    height: 165,
+    desired: 56,
+  },
+  {
+    userId: 3,
+    name: 'user3',
+    age: 45,
+    blood: 1,
+    current: 90,
+    height: 174,
+    desired: 60,
+  },
+];
