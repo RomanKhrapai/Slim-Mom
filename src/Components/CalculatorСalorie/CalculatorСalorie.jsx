4c5e44909b90d331b5ca597e2cdf6f3ebb278545
PiekharievaK <pechareva@gmail.com>
2022-07-09 00:54:28 +0300

    убирает пользователей из компонента и импортирует временную БД

--- src/Components/CalculatorСalorie/CalculatorСalorie.jsx
+++ src/Components/CalculatorСalorie/CalculatorСalorie.jsx
@@ -1,28 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import DailyCaloriesForm from 'Components/DailyCaloriesForm';
 import UserInfo from './UserInfo';
-
+import users from '../usersDB';
 // Имитация базы и поиска пользователя, для проверки работоспособности
-const users = [
-  {
-    userId: 1,
-    name: 'user1',
-    age: 27,
-    blood: 2,
-    current: 75,
-    height: 170,
-    desired: 65,
-  },
-  {
-    userId: 2,
-    name: 'user2',
-    age: 20,
-    blood: 4,
-    current: 80,
-    height: 165,
-    desired: 56,
-  },
-];
+
 const userId = users[0].userId;
 const user = users.find(user => user.userId === userId);
 
