199bb67db04024d02df352b08f4c768e8368287d
RomanKhrapai <hrapayr@gmail.com>
2022-07-14 16:31:37 +0300

    +

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,25 +1,25 @@
-import React, {useState, useEffect} from "react"
-import styles from './userinfo.module.scss'
+import React, { useState, useEffect } from 'react';
+import styles from './userinfo.module.scss';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import {logOut} from '../../redux/auth/auth-operations'
+import * as logOut from '../../redux/auth/auth-operations';
+//import {logOut} from '../../redux/auth/auth-operations'
 
 const UserInfo = () => {
-
-  const { user } = useSelector(state => state)
-  const dispatch = useDispatch()
+  const { user } = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const tryLogOut = async () => {
-    dispatch(logOut())
-  }
+    dispatch(logOut());
+  };
 
   return (
-      <div className={styles.userinfo}>
-        <h3>{user.name === '' ? 'Name' : user.name}</h3>
-        <div className={styles.vector1} />
-        <button onClick={() => tryLogOut()}>Sign Out</button>
-      </div>
-  )
-}
+    <div className={styles.userinfo}>
+      <h3>{user.name === '' ? 'Name' : user.name}</h3>
+      <div className={styles.vector1} />
+      <button onClick={() => tryLogOut()}>Sign Out</button>
+    </div>
+  );
+};
 
-export default UserInfo
+export default UserInfo;
