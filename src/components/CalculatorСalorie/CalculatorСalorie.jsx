55f6734c2698e5c697662f468eb9ce2ec40c6457
PiekharievaK <pechareva@gmail.com>
2022-07-15 08:30:50 +0300

    Правит стили калькулятора

--- src/components/CalculatorСalorie/CalculatorСalorie.jsx
+++ src/components/CalculatorСalorie/CalculatorСalorie.jsx
@@ -8,16 +8,16 @@ import users from '../usersDB';
 import style from '../DailyCaloriesForm//DailyCaloriesForm.module.scss';
 
 // Имитация базы и поиска пользователя, для проверки работоспособности
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 // import productsOperations from '../../redux/products/products-operation';
-// import userOperations from 'redux/user/user-operation';
-// import authOperations from 'redux/auth/auth-operations';
+import userOperations from 'redux/user/user-operation';
+import authOperations from 'redux/auth/auth-operations';
 
 const сalculatorСalorie = () => {
   // функція для перекладу
   const { t } = useTranslation();
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   // Активность режима редактирования
   const userId = 2;
   const fetchUser = users.find(user => user.userId === userId);
@@ -28,9 +28,11 @@ const сalculatorСalorie = () => {
 
   const changeActive = () => {
     // dispatch(productsOperations.getAllProducts());
+    // dispatch(authOperations.logIn({email: "user2test@gmail.com", password: "user2test" }));
+    // dispatch(authOperations.signUpUser({ name: "user37Test", email: "user37test@gmail.com", password: "user37test" }));
+  
+    // dispatch(userOperations.addProductToDiary({date: "29299292", productId: "5d51694802b2373622ff553b", amount: 500, }))
     // dispatch(userOperations.getDayProducts());
-    // dispatch(userOperations.getUser());
-    // dispatch(authOperations.fetchCurrentUser());
     setActiveModerate(!activeModerate);
   };
 
@@ -44,13 +46,15 @@ const сalculatorСalorie = () => {
   if (!activeModerate) {
     return (
       <>
-        <h2>{t('calculator.Your Parametrs')}</h2>
+        <h2 className={s.title}>{t('calculator.Your Parametrs')}:</h2>
+        <div>
         <UserInfo userData={user} />
         <Button type="button" className={style.Button} onClick={changeActive}>
           {fetchUser
             ? t('calculator.Change information')
             : t('calculator.Add information')}
         </Button>
+            </div>
         {/* <Button  type="button" disabled={fetchUser? false: true} >
           {t("calculator.View your losing weight plan")}
           </Button > */}
@@ -59,9 +63,9 @@ const сalculatorСalorie = () => {
   }
 
   return (
-    <div className={s.Box}>
+    <div className={s.box}>
       <DailyCaloriesForm userData={user} />
-      <Button type="button" onClick={changeActive} className={s.Button}>
+      <Button type="button" onClick={changeActive} className={s.button}>
         {t('calculator.Close Changes')}
       </Button>
     </div>
