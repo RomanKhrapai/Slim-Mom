5474bad77098a32a5d57f4508595bf1ee711cb1d
PiekharievaK <pechareva@gmail.com>
2022-07-15 15:33:30 +0300

    Добавляет запись данных юзера после входа, в калькулятор

--- src/redux/auth/auth-reducer.js
+++ src/redux/auth/auth-reducer.js
@@ -5,7 +5,7 @@ const authSlice = createSlice({
   name: 'auth',
   initialState: {
     isAuthorised: false,
-    user: { name: '', email: '', id: '', refreshToken: '' },
+    user: { name: '', email: '', id: '', refreshToken: '', params:{} },
     isLoading: false,
   },
   reducers: {},
@@ -14,7 +14,7 @@ const authSlice = createSlice({
       state.isLoading = true;
     },
     [authOperations.signUpUser.fulfilled]: (state, action) => {
-      // state.user.id = action.payload.data.user.id
+      state.user.params = action.payload.data.user.params
       state.user.name = action.payload.user.name;
       state.user.email = action.payload.email;
       state.user.refreshToken = action.payload.refreshToken;
@@ -39,6 +39,7 @@ const authSlice = createSlice({
       state.isLoading = true;
     },
     [authOperations.logIn.fulfilled]: (state, action) => {
+      state.user.params = action.payload.user.params,
       state.user.name = action.payload.user.name;
       state.user.email = action.payload.user.email;
       state.user.refreshToken = action.payload.refreshToken;
