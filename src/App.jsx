d08455f84a68d395682185367492f45e2517f23d
RomanKhrapai <hrapayr@gmail.com>
2022-07-15 14:38:16 +0300

    delete base name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { React, lazy, Suspense, useState, useContext, useEffect} from 'react';
+import { React, lazy, Suspense, useState, useContext, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -13,7 +13,6 @@ import Loader from 'components/Loader';
 // import { ThemeContext } from 'components/ThemeProvider/ThemeProvider';
 // import { BsSun,BsMoon } from 'react-icons/bs';
 
-
 const LoginView = lazy(() => import('./pages/LoginPage/LoginPage'));
 const RegisterView = lazy(() => import('./pages/RegisterPage/RegisterPage'));
 const MainPage = lazy(() => import('./pages/MainPage'));
@@ -21,21 +20,18 @@ const DiaryPage = lazy(() => import('./pages/DiaryPage/DiaryPage'));
 const CalculatorPage = lazy(() => import('./pages/CalculatorPage'));
 
 export const App = () => {
-
   const isLoggedIn = true;
-//   const [{theme, isDark}, toggleTheme] = useContext(ThemeContext)
-//   const [icon, setIcon] = useState(<BsSun size={40}/>)
+  //   const [{theme, isDark}, toggleTheme] = useContext(ThemeContext)
+  //   const [icon, setIcon] = useState(<BsSun size={40}/>)
 
-  
   const isAuthorised = useSelector(state => state.auth.isAuthorised);
 
-
-// useEffect(()=>{
-//   if(isDark){
-//     setIcon(<BsMoon size={40}/>)
-//   }
-//   else{setIcon(<BsSun size={40}/>)}
-// },[isDark])
+  // useEffect(()=>{
+  //   if(isDark){
+  //     setIcon(<BsMoon size={40}/>)
+  //   }
+  //   else{setIcon(<BsSun size={40}/>)}
+  // },[isDark])
 
   const { t } = useTranslation();
 
@@ -51,46 +47,50 @@ export const App = () => {
 
   return (
     // <div className={showModal ? s.overflow_hidden : undefined} style={{backgroundColor: theme.backgroundColor, color: theme.color}}>
-        <div className={showModal ? s.overflow_hidden : undefined}> 
-      <BrowserRouter basename={'Slim-Mom'}>
-            <Header />
-            {/* <div onClick={toggleTheme}>{icon}</div> */}
-              <Suspense fallback={<div>LOADER</div>}>
-                {isAuthorised ? (
-                  <Routes>
-                    <Route path={'/'} element={<MainPage toggleModal={toggleModal} showModal={showModal}/>} />
-                    <Route path={'/diary'} element={<DiaryPage />} />
-                    <Route path={'/calculator'} element={<CalculatorPage />} />
-                    <Route
-                      path={'*'}
-                      replace={true}
-                      element={<Navigate to={'/'} />}
-                    />
-                  </Routes>
-                ) : (
-                  <Routes>
-                    <Route
-                      path={'/'}
-                      element={
-                        <h1 style={{ marginTop: '200px' }}>
-                          {t('Calculate your daily calorie intake')}
-                        </h1>
-                      }
-                    />
-                    <Route path={'/registration'} element={<RegisterView />} />
-                    <Route path={'/login'} element={<LoginView />} />
+    <div className={showModal ? s.overflow_hidden : undefined}>
+      <BrowserRouter>
+        <Header />
+        {/* <div onClick={toggleTheme}>{icon}</div> */}
+        <Suspense fallback={<div>LOADER</div>}>
+          {isAuthorised ? (
+            <Routes>
+              <Route
+                path={'/'}
+                element={
+                  <MainPage toggleModal={toggleModal} showModal={showModal} />
+                }
+              />
+              <Route path={'/diary'} element={<DiaryPage />} />
+              <Route path={'/calculator'} element={<CalculatorPage />} />
+              <Route
+                path={'*'}
+                replace={true}
+                element={<Navigate to={'/'} />}
+              />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route
+                path={'/'}
+                element={
+                  <h1 style={{ marginTop: '200px' }}>
+                    {t('Calculate your daily calorie intake')}
+                  </h1>
+                }
+              />
+              <Route path={'/registration'} element={<RegisterView />} />
+              <Route path={'/login'} element={<LoginView />} />
 
-                    <Route
-                      path={'*'}
-                      replace={true}
-                      element={<Navigate to={'/registration'} />}
-                    />
-                  </Routes>
-                )}
-              </Suspense>
-
+              <Route
+                path={'*'}
+                replace={true}
+                element={<Navigate to={'/registration'} />}
+              />
+            </Routes>
+          )}
+        </Suspense>
       </BrowserRouter>
-      
+
       <ToastContainer autoClose={3000} />
     </div>
   );
