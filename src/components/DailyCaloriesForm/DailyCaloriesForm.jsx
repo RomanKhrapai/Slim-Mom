f5e1b4bdcfce5822d0d2521e4674a6d4a7bcadc7
PavlinaAntipova <44255589+PavlinaAntipova@users.noreply.github.com>
2022-07-16 21:36:08 +0300

    Update DailyCaloriesForm.jsx

--- src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
+++ src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
@@ -126,16 +126,8 @@ const DailyCaloriesForm = ({
           const valuesWithLanguage = { ...convertedType, language };
 
           if (isAuthorised) {
-            const toNumberValues = changeType(values);
-            const newUserValues = {
-              height: toNumberValues.height,
-              age: toNumberValues.age,
-              currentWeight: toNumberValues.currentWeight,
-              desiredWeight: toNumberValues.desiredWeight,
-              bloodType: toNumberValues.bloodType,
-              language
-            };
-            dispatch(userOperations.addUserInfo(convertedType)).then(() => {
+            
+            dispatch(userOperations.addUserInfo(valuesWithLanguage)).then(() => {
 
               onOpenModal();
               dispatch(apdateUserInfo(valuesWithLanguage));
